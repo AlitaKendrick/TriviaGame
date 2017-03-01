50f77682aaeedd4b2a37014721c4be557995b04f
Alita Kendrick <alitakendrick@Alitas-MacBook-Pro.local>
2017-02-28 18:34:51 -0600

    starting out

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){ 
+
+
+//the game object that contains all questions/answers
+//add more questions later
+var setup = {
+	questions: [
+		{
+			question: "",
+			answer1: "",
+			answer2: "",
+			answer3: "",
+			correct: "",
+			image: ""
+		},
+		{
+			question: "",
+			answer1: "",
+			answer2: "",
+			answer3: "",
+			correct: "",
+			image: ""
+		},
+		{
+			question: "",
+			answer1: "",
+			answer2: "",
+			answer3: "",
+			correct: "",
+			image: ""
+		},
+
+]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
